63dee9aaec15354656c45ab535cba3b00da8775b
saikishan <saikishan2008@gmail.com>
2017-02-17 14:39:05 +0530

    waterfall learnt

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ server.post('api/messages',connector.listen());
 //=====
 //==bot Dialogs
 //==========
-bot.dialog('/',function(session){
-    session.send("hello world");
-});
+bot.dialog('/',[
+    function(session){
+        session.beginDialog('\ensure profile',session.userData.profile);
+    },
+    function(session,results){
+        session.userData.profile= results.response;
+        session.send("hiii %(name)s u r ID is %(ID)s\n ur age is %(age)d\n",session.userData.profile);
+    }
+]);
+bot.dialog('\ensure profile',[function(session,args,next){
+    session.dialogData.profile =args || {};
+    if(!session.dialogData.profile.name)  builder.Prompts.text(session,"hello what is your name ?");
+    else next();
+},function(session,results,next){
+    if(results.response)    session.dialogData.profile.name = results.response;
+    if(!session.dialogData.profile.ID)   builder.Prompts.text(session,"enter the ID number ");
+    else next();
+},function(session,results,next){
+    if(results.response)    session.dialogData.profile.ID = results.response;
+    if(!session.dialogData.profile.age) builder.Prompts.number(session,"enter the age")
+    else next()
+},function(session,results){
+    if(results.response)   session.dialogData.profile.age = results.response;
+    session.endDialogWithResult( { response: session.dialogData.profile });
+}]);
