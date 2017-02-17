29ee19148a16364f30884fff85a66865087d8173
saikishan <saikishan2008@gmail.com>
2017-02-17 18:26:37 +0530

    waterfall learnt

--- app.js
+++ app.js
@@ -25,11 +25,6 @@ bot.dialog('/',[
     function(session,results){
             session.userData.profile= results.response;
             session.send("hiii %(name)s u r ID is %(ID)s\n ur age is %(age)d\n",session.userData.profile);
-            session.beginDialog('\get scores',session.userData.scores); 
-    },
-    function(session,results){
-        session.userData.scores = results.responsescore;
-        session.send("hii %(name)s u r ID is %(ID)s u r age is %(age)d\n your score is %(y1)d",session.userData.profile,session.userData.scores);
     }
 ]);
 bot.dialog('\ensure profile',[function(session,args,next){
@@ -42,21 +37,9 @@ bot.dialog('\ensure profile',[function(session,args,next){
     else next();
 },function(session,results,next){
     if(results.response)    session.dialogData.profile.ID = results.response;
-    if(!session.dialogData.profile.age) builder.Prompts.number(session,"enter the age")
+    if(!session.dialogData.profile.age) builder.Prompts.number(session,"enter the age");
     else next()
 },function(session,results){
     if(results.response)   session.dialogData.profile.age = results.response;
     session.endDialogWithResult( { response: session.dialogData.profile });
 }]);
-bot.dialog('\get scores',[function(session,args,next){
-    session.dialogData.scores =args||{};
-  //  if(!session.dialogData.scores.y1)
-        builder.Prompts.number(session,"enter the year 1 scores");
-    //else next();    
-},
-function(session ,results){
-    if(results.response)   session.dialogData.scores.y1 = results.response;
-    console.log(session.dialogData.scores.y1);
-    session.endDialogWithResult({ responsescore: session.dialogData.scores  });
-}
-]);
